f9de7985950517f883ecba0bc5c5dd6d9714c6c9
finnmacke <finn@pikupstix.co.uk>
2026-07-05 16:40:51 +0100

    Positions rep added

---
+++ IKBR_Report_Puller/Interfaces/IPositionRepository.cs
@@ -0,0 +1,19 @@
+using IKBR_Report_Puller.Domain;
+
+namespace PikUpStix.TraderView.Interfaces
+{
+    public interface IPositionRepository
+    {
+        /// <summary>
+        /// Gets all positions from the database
+        /// </summary>
+        /// <returns>List of positions</returns>
+        List<Position> GetAllPositions();
+        /// <summary>
+        /// Inserts a list of positions into the database
+        /// </summary>
+        /// <param name="positions">List of positions to insert</param>
+        void InsertPositions(List<Position> positions);
+    }
+}
+    
